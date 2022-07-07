1df94a694383caa9672f012009b8740187caaf43
miaccc <ta828383@gmail.com>
2022-07-08 01:33:29 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,27 @@
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index')
+})
+// app.get('/about', (req, res) => {
+//   res.render('show')
+// })
+app.get('/:button', (req, res) => {
+  res.render('show', { button: req.params.button })
+  
+})
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
